39a35916317f174929a958f4fc56b86162242707
Sadvikkr <sadvikkumar09h@gmail.com>
2026-06-21 21:17:28 +0530

    Added String functions

---
+++ Python Learning/01-fundamentals/14_strings.py
@@ -0,0 +1,17 @@
+# lower() - Converts to lower case 
+# upper() - Converts to upper case
+# title() - Capitalizes first letter of every word.
+# capitalize() - Only first letter of entire string.
+# strip() - Removes spaces from both ends
+# lstrip() - Remove left spaces.
+# rstrip() - Remove right spaces.
+# replace() - Replace text.
+# split() - Convert string → list.
+# join() - Convert list → string.
+# find() - Find position.
+# count() - Count occurrences
+# startswith() 
+# endswith()
+# isdigit() - Checks if all characters are digits.
+# isalpha() - Checks if all characters are letters.
+# isalnum() - Letters + numbers only
