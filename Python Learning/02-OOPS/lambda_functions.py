bf9d0b95d97d8639cf1e11bb6058f13aa8075407
Sadvikkr <sadvikkumar09h@gmail.com>
2026-06-23 20:36:48 +0530

    Added Lambda Functions

---
+++ Python Learning/02-OOPS/lambda_functions.py
@@ -0,0 +1,27 @@
+# # Normal Way 
+
+# def square(num):
+#     return num*num
+
+# # Using Lambda function
+
+# square = lambda num: num*num
+
+# print(square(5))
+
+# EX -1 (Add two numbers)
+
+add = lambda a,b: a+b
+print(add(10,20))
+
+# EX - 2
+
+multiply = lambda a,b : a*b
+
+print(multiply(4,5))
+
+# EX - 3 
+
+double = lambda x: x*2
+
+print(double(7))
