bb02048a61f81ae415b92f764b6e3c272e8f3f95
Sadvikkr <sadvikkumar09h@gmail.com>
2026-06-25 14:54:50 +0530

    Implemented POST endpoint with automatic student ID generation

--- Python Learning/05-FastAPI/student_api.py
+++ Python Learning/05-FastAPI/student_api.py
@@ -8,28 +8,48 @@ class Student(BaseModel):
 
 app = FastAPI()
 
+students = [
+    {
+        "id": 1,
+        "name": "Sadvik",
+        "age": 19,
+        "marks": 89
+    },
+    {
+        "id": 2,
+        "name": "Aman",
+        "age": 20,
+        "marks": 95
+    }
+]
+
 @app.get("/")
 def home():
     return {"message" : "Student API Running"}
 
 @app.get("/students")
 def get_students():
-    return[
-        {"id": 1, "name": "Sadvik", "marks": 89},
-        {"id": 2, "name": "Aditya", "marks": 78}
-    ]
+    return students
+    
 
 @app.get("/students/{id}")
 def get_student(id: int):
-    return{
-        "id": id,
-        "name": "Sadvik",
-        "marks": 89
-    }
-
+    
+    for student in students:
+        if student["id"] == id:
+            return student
+          
 @app.post("/students")
 def create_student(student: Student):
+    student_data = student.model_dump()  # student.dict() can also be used 
+    next_id = len(students) + 1
+    student_data["id"] = next_id
+
+    
+    students.append(student_data)   
     return {
         "message": "Student Created",
-        "student": student
+        "student": student_data
     }
+
+    
